47a74b1880ff8bfd098dbe8f4e9f0210c5de6c53
Raman Singh <kumarraman1694@gmail.com>
2025-08-18 18:17:39 +0530

    Create Q5_customers_over_100.sql

---
+++ 1_Foundation/03_SQLITE/Project/Q5_customers_over_100.sql
@@ -0,0 +1,6 @@
+SELECT c.customer_id, c.first_name, c.last_name, SUM(p.amount) AS total_spent
+FROM customer c
+JOIN payment p ON c.customer_id = p.customer_id
+GROUP BY c.customer_id
+HAVING SUM(p.amount) > 100
+ORDER BY total_spent DESC;
